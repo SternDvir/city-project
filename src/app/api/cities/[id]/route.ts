a1600dc50c33219af08c51c57f361fdf04e7779c
SternDvir <sterndvir1@gmail.com>
2025-09-25 15:47:53 +0300

    bug fix

--- src/app/api/cities/[id]/route.ts
+++ src/app/api/cities/[id]/route.ts
@@ -1,11 +1,10 @@
-// src/app/api/cities/[id]/route.ts
-import { NextResponse, NextRequest } from "next/server";
+import { NextResponse, type NextRequest } from "next/server";
 import { connectToDatabase } from "@/lib/mongodb";
 import City from "@/models/City";
 
 // DELETE handler for a specific city by its ID
 export async function DELETE(
-  req: NextRequest,
+  request: NextRequest,
   { params }: { params: { id: string } }
 ) {
   try {
